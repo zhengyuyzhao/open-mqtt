eadeb35eca59fb5f1d513b06c2519248ed22063f
zhengyuyzhao <943470549@qq.com>
2025-08-14 10:08:28 +0800

    cassandra

--- mqtt-store-ignite/src/main/java/com/zzy/mqtt/store/ignite/IgniteMessageIdService.java
+++ mqtt-store-ignite/src/main/java/com/zzy/mqtt/store/ignite/IgniteMessageIdService.java
@@ -5,13 +5,21 @@ import lombok.RequiredArgsConstructor;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteAtomicSequence;
 
+import java.util.WeakHashMap;
+
 @RequiredArgsConstructor
 public class IgniteMessageIdService implements IMessageIdService {
     private final Ignite ignite;
+    private WeakHashMap<String, IgniteAtomicSequence> clientIdSeqMap = new WeakHashMap<>();
+
 
     @Override
     public int getNextMessageId(String clientId) {
-        IgniteAtomicSequence count = ignite.atomicSequence(clientId, 1, true);
+        IgniteAtomicSequence count = clientIdSeqMap.get(clientId);
+        if (count == null) {
+            count = ignite.atomicSequence(clientId, 1, true);
+            clientIdSeqMap.put(clientId, count);
+        }
         return (int) ((count.incrementAndGet() % 65530 + 65530) % 65530) + 1;
     }
 
